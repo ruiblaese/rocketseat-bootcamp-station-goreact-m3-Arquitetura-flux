58096870566d88bb6a6230788adbfac9d7085410
Rui Richard Blaese <ruiblaese@gmail.com>
2019-05-13 21:41:09 -0300

    finalizando projeto de testes

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,21 +1,31 @@
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import api from "../../services/api";
 
-import { addFavoriteSuccess, addFavoriteFailure } from "../actions/favorites";
+import { Creators as FavoriteActions } from "../ducks/favorites";
 
 export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url
-    };
+    const isDuplicated = yield select(state =>
+      state.favorites.data.find(favorite => favorite.id === data.id)
+    );
 
-    yield put(addFavoriteSuccess(repositoryData));
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure("repositorio duplicado"));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url
+      };
+
+      yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (error) {
-    yield put(addFavoriteFailure("Erro ao adicionar repositorio"));
+    yield put(
+      FavoriteActions.addFavoriteFailure("Erro ao adicionar repositorio")
+    );
   }
 }
